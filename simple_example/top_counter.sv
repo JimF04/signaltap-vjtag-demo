bc319f615bbf3175d3944283c835dce41c08281c
Jimmy Feng Feng <jimmy.feng04@gmail.com>
2026-09-25 23:08:07 -0600

    [UPDATE] Ejemplo simple (contador con boton)

---
+++ simple_example/top_counter.sv
@@ -0,0 +1,30 @@
+module top_counter (
+    input  logic       clk,     // Reloj de la FPGA (50 MHz)
+    input  logic       rst,     // Reset
+	 input  logic       btn_inc, // Botón de incremento
+    output logic [7:0] led    // Contador binario de 8 bits 
+);
+
+    // btn_pulse: pulso de 1 ciclo cuando el botón se presiona de forma válida
+    logic btn_pulse;
+ 
+    debounce #(.N(800000)) u_debounce (
+        .clk (clk),
+        .rst (rst),
+        .ent (btn_inc),
+        .out (btn_pulse)
+    );
+	 
+    // Contador de 8 bits
+    logic [7:0] count;
+ 
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst)
+            count <= 8'd0;
+        else if (btn_pulse)
+            count <= count + 1'b1;
+    end
+ 
+    assign led = count;
+
+endmodule 
